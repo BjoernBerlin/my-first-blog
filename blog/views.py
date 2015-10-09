b14815a5970f5f7e7887c85b407f7a1d4ba4e7b6
Bjoern Lengers <bjoern@lengers.de>
2015-10-09 15:46:39 +0200

    Neuer Post, Drafts, Editieren, Publishen und Löschen hinzugefügt.

--- blog/views.py
+++ blog/views.py
@@ -36,4 +36,18 @@ def post_edit(request, pk):
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})        
+    return render(request, 'blog/post_edit.html', {'form': form}) 
+    
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')  #auch hier umgedreht
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+    
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')    
